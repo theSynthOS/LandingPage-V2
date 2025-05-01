97f7c8cab3deea6daadc645dbf027cb980474587
cheongyeechian <cheongyeechian@gmail.com>
2025-05-01 12:15:28 +0800

    mobile navbar and design

--- src/component/sections/HeroSection.tsx
+++ src/component/sections/HeroSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import GlowButton from '../GlowButton';
 
 const HeroSection = () => {
   return (
@@ -10,10 +11,10 @@ const HeroSection = () => {
          </video>
        </div>
       <div className="container mx-auto px-4 relative z-10">
-        <div className="max-w-6xl mx-auto text-center">
-          <h1 className="text-[2rem] md:text-[4rem] lg:text-[5rem] xl:text-[6rem] font-bold mb-6 bg-clip-text text-white" style={{fontFamily: 'Montserrat-LightItalic'}}>No-code </h1>
-          <h1 className="text-[2rem] md:text-[4rem] lg:text-[5rem] xl:text-[6rem] font-bold mb-6 bg-clip-text text-white text-nowrap " style={{fontFamily: 'Montserrat-LightItalic'}}>
-            <span className="text-white" style={{ textShadow: '0 0 1px blue, 0 0 20px yellow, 0 0 10px orange' }}>AI agents </span> for DeFi
+        <div className="max-w-6xl mx-auto text-center mt-40 md:mt-0">
+          <h1 className="text-[3rem] md:text-[4rem] lg:text-[5rem] xl:text-[6rem] font-bold mb-6 bg-clip-text text-white" style={{fontFamily: 'Montserrat-LightItalic'}}>No-code </h1>
+          <h1 className="text-[3rem] md:text-[4rem] lg:text-[5rem] xl:text-[6rem] font-bold mb-6 bg-clip-text text-white " style={{fontFamily: 'Montserrat-LightItalic'}}>
+            <span className="text-white" style={{ textShadow: '0 0 10px white, 0 0 40px yellow, 0 0 30px orange' }}>AI agents </span> for DeFi
           </h1>
           <p className="font-montserrat text-xl md:text-2xl text-white mb-8 mt-14">
             SynthOS makes DeFi plug-and-play. Deploy intelligent, verifiable agents that automate your protocol logic in secure enclaves. Let users express intent, and let agents do the rest.
@@ -21,8 +22,8 @@ const HeroSection = () => {
           
           {/* Launch App Button - Visible only on mobile */}
           <div className="mt-8 md:hidden">
-            <Link href="#" className="inline-block px-10 py-3 rounded-full bg-transparent text-white border border-gray-600 hover:bg-gray-800 transition duration-300">
-              Launch App
+            <Link href="https://app.synthos.fun" className='' >
+              <GlowButton>Launch App</GlowButton>
             </Link>
           </div>
         </div>
