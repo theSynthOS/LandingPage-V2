bd4e8b14edbe294c9ef18345c85afefeb0f9c476
Yudhishthra Sugumaran <66850474+0xYudhishthra@users.noreply.github.com>
2025-05-29 17:58:49 +0800

    feat: permanent redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://app.synthos.fun",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
