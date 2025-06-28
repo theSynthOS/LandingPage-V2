c598258003c0c1470e18c772eae23c84a2654da9
cheongyeechian <cheongyeechian@gmail.com>
2025-06-28 18:18:36 +0800

    Update HeroSection.tsx

--- src/component/sections/HeroSection.tsx
+++ src/component/sections/HeroSection.tsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
       <div className="container mx-auto px-4 relative z-10">
         <div className="max-w-6xl mx-auto text-center mt-40 md:mt-0">
           <p className="font-montserrat text-xl md:text-2xl text-white mb-8 mt-14">
-            Invest with confidence using 
+            Invest with confidence  
           </p>
           <h1 className="text-[2.3rem] md:text-[4rem] lg:text-[5rem] xl:text-[5rem] text-center font-bold mb-6 bg-clip-text text-white uppercase" style={{fontFamily: 'Montserrat-LightItalic'}}><span className="text-white" style={{ textShadow: '0 0 10px white, 0 0 40px yellow, 0 0 30px orange' }}>Personalized </span>crypto yield plans</h1>
           {/* Launch App Button - Visible only on mobile */}
